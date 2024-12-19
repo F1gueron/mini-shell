15d5d7e503ba46ff337bb270eab26270406b5d8d
F1gueron <jfiguerasmarquez@gmail.com>
2024-12-19 19:41:01 +0100

    - Formatted (again)

--- minishell.c
+++ minishell.c
@@ -8,24 +8,13 @@
 #include <errno.h>
 #include "parser.h"
 #include <fcntl.h>
+
 #define MAX_BG_PROCESS 1024 //Número máximo de procesos que vamos a poder ejecutar en segundo plano a la vez
-
-
-
 
 # define MAX_INPUT 1024
 
-#define RESET      "\033[0m"
-#define RED        "\033[31m"
-#define GREEN      "\033[32m"
-#define YELLOW     "\033[33m"
-#define BLUE       "\033[34m"
-
-
-
 pid_t bg_pids[MAX_BG_PROCESS];//Array de pids
 int contador = 0;//inicializamos variable contador que contará los procesos corriendo en background
-
 
 void addBgProcessArray(pid_t pid) {
     if (contador < MAX_BG_PROCESS) {
@@ -34,22 +23,6 @@ void addBgProcessArray(pid_t pid) {
         fprintf(stderr, "Error: no se pueden agregar más procesos en background (límite alcanzado).\n");
     }
 }
-
-void removeBgProcessArray(pid_t pid) {
-    for (int i = 0; i < contador; i++) {
-        if (bg_pids[i] == pid) {
-            // Mueve los elementos restantes hacia adelante
-            for (int j = i; j < contador - 1; j++) {
-                bg_pids[j] = bg_pids[j + 1];
-            }
-            contador--;
-            return;
-        }
-    }
-    fprintf(stderr, "Error: PID %d no encontrado en los procesos en background.\n", pid);
-}
-
-
 
 void execute_background(const char *command, char **args) {
     pid_t pid = fork();
@@ -73,6 +46,19 @@ void execute_background(const char *command, char **args) {
     }
 }
 
+// Manejador para limpiar procesos terminados en background
+void manejador_sigchld(int sig) {
+    (void)sig; // Evitar advertencias sobre argumentos no usados
+    pid_t pid;
+    int estado;
+
+    // Limpiar todos los procesos terminados
+    while ((pid = waitpid(-1, &estado, WNOHANG)) > 0) {
+        printf("Proceso en background (PID: %d) terminado.\n", pid);
+        removeBgProcessArray(pid);
+    }
+}
+
 void jobs() {
     printf("Procesos en background:\n");
     for (int i = 0; i < contador; i++) {
@@ -91,18 +77,16 @@ void jobs() {
 
 void handle_signal(int sig) {
     if (sig == SIGINT) {
-        printf("\n" YELLOW "SIGINT received.\n" RESET); // TODO: Implement handling for process in foreground
-        // TODO
+        printf("\n"); 
     } else if (sig == SIGQUIT) {
-        printf("\n" YELLOW "SIGQUIT received.\n" RESET); // TODO: Implement handling for process in foreground.
-        // TODO
+        printf("\n"); 
     }
 }
 
 void cd(char *path) {
     char resolved_path[1024];
 
-    if (path == NULL || strcmp(path, "~") == 0) { 
+    if (path == NULL) { 
         path = getenv("HOME"); // Ir al directorio HOME si no se proporciona un argumento o es "~"
     } else if (path[0] == '~') { 
         // Cuando la dirección es "~/*"
@@ -126,8 +110,9 @@ void execute_command(tcommand *comando, int fd_in, int fd_out) {
         dup2(fd_out, 1);
         close(fd_out);
     }
-    execvp(comando->argv[0], comando->argv); // Buscar en el PATH
-    fprintf(stderr, RED "Error: No se pudo ejecutar '%s': %s\n" RESET, comando->argv[0], strerror(errno));
+    if (execvp(comando->argv[0], comando->argv) == -1) {
+        fprintf(stderr,"%s: No se encuentra el mandato\n" , comando->argv[0]);
+    }
 }
 
 void execute_piped_commands(tline *line) {
@@ -137,13 +122,17 @@ void execute_piped_commands(tline *line) {
     int fd_out = 1;
 
     for (i = 0; i < line->ncommands; i++) {
+        if (line->commands[i].filename == NULL) {
+            fprintf(stderr,"%s: No se encuentra el mandato\n", line->commands[i].argv[0]);
+            break;
+        }
         if (pipe(pipefd) == -1) {
             perror("pipe");
             exit(EXIT_FAILURE);
         }
 
         // Ningún built-in tiene que hacer fork
-        // Como un hijo no puede cambiar el directorio de trabajo de un padre, se maneja el comando cd aquí
+        // Como un hijo no puede cambiar el directorio de trabajo de un padre, se maneja el comando `cd` aquí
         if (strcmp(line->commands[i].argv[0], "cd") == 0) {
             cd(line->commands[i].argv[1]);
             close(pipefd[0]);
@@ -156,46 +145,49 @@ void execute_piped_commands(tline *line) {
             close(pipefd[1]);
             continue;
         }
+
         pid_t pid = fork();
         if (pid == -1) {
             perror("fork");
         }
 
         if (pid == 0) { // Proceso hijo
-            if (fd_in != 0) {
-                dup2(fd_in, 0); 
+            signal(SIGINT, SIG_DFL);
+            signal(SIGQUIT, SIG_DFL);
+            if (fd_in != 0) { // No es el primer comando
+                dup2(fd_in, 0); // Redirigir la entrada estándar al pipe de lectura
                 close(fd_in);
             }else{
                 if (line->redirect_input != NULL){
                     int fd_in = open(line->redirect_input, O_RDONLY);
                     if (fd_in == -1) {
-                        perror("open");
+                        fprintf(stderr, "%s Error.%s\n", line->redirect_input, strerror(errno));
                         continue;
                     }
-                    dup2(fd_in, STDIN_FILENO);
+                    dup2(fd_in, STDIN_FILENO); // Redirigir la entrada estándar al archivo
                     close(fd_in);
                     }
             }
-            if (i < line->ncommands - 1) {
+            if (i < line->ncommands - 1) { // No es el último comando
                 dup2(pipefd[1], 1);
                 close(pipefd[1]);
             } else {
                 if (line->redirect_output != NULL) {
                     fd_out = open(line->redirect_output, O_WRONLY | O_CREAT | O_TRUNC, 0644);
                     if (fd_out == -1) {
-                        perror("open");
+                        fprintf(stderr, "%s Error.%s\n", line->redirect_output, strerror(errno));
                         exit(EXIT_FAILURE);
                     }
-                    dup2(fd_out, STDOUT_FILENO);
+                    dup2(fd_out, STDOUT_FILENO); // Redirigir la salida estándar al archivo
                     close(fd_out);
                 }
-                if (line->redirect_error != NULL) {
+                if (line->redirect_error != NULL) { 
                     int fd_err = open(line->redirect_error, O_WRONLY | O_CREAT | O_TRUNC, 0644);
-                    if (fd_err == -1) {
-                        perror("open");
+                    if (fd_err == -1) { 
+                        fprintf(stderr, "%s Error.%s\n", line->redirect_error, strerror(errno));
                         exit(EXIT_FAILURE);
                     }
-                    dup2(fd_err, STDERR_FILENO);
+                    dup2(fd_err, STDERR_FILENO); // Redirigir la salida de error estándar al archivo
                     close(fd_err);
                 }
             }
@@ -222,30 +214,21 @@ void display_prompt() {
         user = "Anonymous";
     }
     if (getcwd(cwd, sizeof(cwd)) != NULL) {
-        printf(BLUE "%s" GREEN "@" BLUE "msh" GREEN ":" BLUE "%s" GREEN "$> ", user, cwd);
+        printf("msh> ");
+        //printf(BLUE "%s" GREEN "@" BLUE "msh" GREEN ":" BLUE "%s" GREEN "$> ", user, cwd);
     } else {
         perror("getcwd");
         exit(EXIT_FAILURE);
     }
 }
-// Manejador para limpiar procesos terminados en background
-void manejador_sigchld(int sig) {
-    (void)sig; // Evitar advertencias sobre argumentos no usados
-    pid_t pid;
-    int estado;
 
-    // Limpiar todos los procesos terminados
-    while ((pid = waitpid(-1, &estado, WNOHANG)) > 0) {
-        printf("Proceso en background (PID: %d) terminado.\n", pid);
-        removeBgProcessArray(pid);
-    }
-}
+
 
 int main() {
     char input[MAX_INPUT];
     tline *line;
-    signal(SIGINT, handle_signal);
-    signal(SIGQUIT, handle_signal);
+    signal(SIGINT, SIG_IGN);
+    signal(SIGQUIT, SIG_IGN);
 
     while (1) {
         display_prompt();
@@ -255,13 +238,13 @@ int main() {
 
         if (strcmp(input, "quit\n") == 0 || strcmp(input, "exit\n") == 0 ||
             strcmp(input, "quit()\n") == 0 || strcmp(input, "exit()\n") == 0) {
-            printf(GREEN "Exiting shell. Goodbye!\n" RESET );
+            printf("Exiting shell. Goodbye!\n");
             return 0;
         }
 
         line = tokenize(input); // Tokenizar la entrada usando librería del profe
-        if (line == NULL) {
-            fprintf(stderr, RED "Error: no se pudo procesar el comando.\n" RESET);
+        if (line->ncommands == 0) {
+            fprintf(stderr,"Error: no se pudo procesar el comando.\n");
             continue;
         }
         
